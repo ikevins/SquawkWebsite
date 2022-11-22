5c889ff64b95010398304ded1d970a9942fcce3f
Jonathan Velez <jonathan14982@gmail.com>
2022-11-22 00:02:43 -0500

    fix user authentication

--- api/middleWare/authMiddleWare.js
+++ api/middleWare/authMiddleWare.js
@@ -2,21 +2,22 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
+
 const protect = asyncHandler(async (req, res, next) => {
     try {
-        const { token } = req.headers;
-        console.log("token:", token);
+        const { token } = req.headers; //grabes token from headers
+        
         if (!token) {
             res.status(401);
             throw new Error("1)Not authorized, please login");
         }
 
-        //verify the token
+        //verify the token with JWT_SECRET
         const verified = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("verified:", verified)
-        //get user id from the token
+
+        //find userid from verified.id 
         const user = await User.findById(verified.id).select("-password");
-        console.log("user:", user)
+
         if (!user) {
             res.status(401);
             throw new Error("user not found");
