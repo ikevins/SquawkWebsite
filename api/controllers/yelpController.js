6358a68ca61a6cd34d10324b0f8a85f4407ccea7
cliffporter <cliff@cliffporter.net>
2022-11-30 18:37:43 -0500

    extra search params

--- api/controllers/yelpController.js
+++ api/controllers/yelpController.js
@@ -9,12 +9,30 @@ const fusionSearch = asyncHandler((req, res) => {
   const limit = req.query.limit;
   const offset = req.query.offset;
   const term = req.query.term;
+  const sort_by = req.query.sort_by;
+  const attributes = req.query.attributes;
+  const open_at = req.query.open_at;
+  const open_now = req.query.open_now;
+  const price = req.query.price;
+  const radius = req.query.radius;
+  const longitude = req.query.longitude;
+  const latitude = req.query.latitude;
   
   client.search({
       location: location,
       offset: offset,
       limit: limit,
-      term: term
+      term: term,
+      sort_by: sort_by,
+      attributes: attributes,
+      open_at: open_at,
+      open_now: open_now,
+      price: price,
+      radius: radius,
+      longitude: longitude,
+      latitude: latitude
+
+
   }).then(response => {
       //console.log(JSON.stringify(response.jsonBody));
       res.send(response.jsonBody.businesses);
