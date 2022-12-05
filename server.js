bfc7eb79691426c4212f1e61acc676d0f1ce145a
Jonathan Velez <jonathan14982@gmail.com>
2022-12-05 17:30:42 -0500

    Another fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   app.get('/*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
