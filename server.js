3257b037f98a829d0c46cc344c88551d3ac55fc1
cliffporter <cliff@cliffporter.net>
2022-12-05 17:37:35 -0500

    something else?

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.use(cors());
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('frontend/build'));
+  // app.use(express.static('frontend/build'));
 
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  app.use(express.static('frontend/build'))
+  app.get('*', (req, res) => {
+  res.sendFile(resolve(__dirname, '..', 'dist', 'index.html'))
+  })
 }
 
 //Api routes for any user function
