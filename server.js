270ee8a490492300fbb8d62a5c6a28e2806b12fe
cliffporter <cliff@cliffporter.net>
2022-12-05 17:44:27 -0500

    fix 5

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static('frontend/build'))
   app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 
