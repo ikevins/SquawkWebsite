510a6b85e51472790f8580c9ad66d874195c1762
cliffporter <cliff@cliffporter.net>
2022-12-07 15:20:37 -0500

    change autologin redirect to /location

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -55,7 +55,7 @@ function Login()
             localStorage.setItem('user_data', JSON.stringify(user));
 
             setMessage('');
-            window.location.href = '/cards';
+            window.location.href = '/location';
 
         }
         catch(e)
